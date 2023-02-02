8bb485719a72aba64fa3da8d502c67f35438c7da
Namita <namitaroy63.nr@gmail.com>
2023-02-01 22:09:09 -0500

    mouse listener and mouse motion listener methods
    adding mouse listener and mouse motion listener methods in the canvas.

--- canvas.java
+++ canvas.java
@@ -7,7 +7,7 @@ import java.awt.event.*;
 // allow the user to clear the canvas
 class canvas extends JFrame implements MouseListener, MouseMotionListener, ActionListener{
 
-    Canvas c;
+    Canvas canvas;
 
     
     
@@ -17,8 +17,8 @@ class canvas extends JFrame implements MouseListener, MouseMotionListener, Actio
 		super("canvas");
 
 		// create a empty canvas
-		c = new Canvas(){
-            public void paint(Graphics g)
+		canvas = new Canvas(){
+            public void paint(Graphics gr)
             {  
             }
         };
@@ -27,15 +27,48 @@ class canvas extends JFrame implements MouseListener, MouseMotionListener, Actio
         // sets size of the frame
 		setSize(500, 500);
         // set background for the canvas
-		c.setBackground(Color.gray);
-
+		canvas.setBackground(Color.gray);
         // set size of the canvas in the frame
-        c.setSize(500, 400);
-		add(c);
+        canvas.setSize(500, 400);
+        // add mouse listener
+        canvas.addMouseListener(this);
+        canvas.addMouseMotionListener(this);
+		add(canvas);
         
         setVisible(true);
 	}
-    
+     // mouse listener  and mouse motion listener methods
+
+     public void mousePressed(MouseEvent e)
+     {
+         Graphics gr = canvas.getGraphics();
+  
+         gr.setColor(Color.red);
+  
+         // get X and y position
+        int x = e.getX();
+        int y = e.getY();
+         // draw a Oval at the point
+         // where mouse is moved
+         gr.fillOval(x, y, 10, 10);
+     }
+     
+     public void mouseDragged(MouseEvent e)
+     {
+         Graphics gr = canvas.getGraphics();
+         Graphics2D g2= (Graphics2D)gr;
+        
+         gr.setColor(Color.red);
+         g2.setStroke(new BasicStroke(5));
+         // get X and y position
+         int x, y;
+         x = e.getX();
+         y = e.getY();
+  
+         // draw an oval with the points where mouse is moved
+         gr.drawLine(x, y, 10, 10);
+         
+     }
  
     public static void main (String[] argv)
     {
